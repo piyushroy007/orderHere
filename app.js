6b9833c1c2a6f15c1f74dc42854cc9e6bd413989
piyushroy007 <piyush9051roy@gmail.com>
2024-06-06 14:16:05 +0530

    inital project setup

---
+++ app.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const AppLayout = () => {
+    return (
+        <div class="app">
+            Welcome to orderHere
+        </div>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
